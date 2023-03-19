60700179b98ce91f971904383099de80cb2064ad
kodedjackson <kodedjackson@gmail.com>
2023-03-19 17:24:13 +0100

    modify config file

--- config.php
+++ config.php
@@ -8,10 +8,12 @@ define('dbHost', 'localhost');
 define('dbUsername','vibrxxrt_playmate');
 define('dbPassword','toyiN080 .');
 define('dbDatabase','kodedchat');
-define('HOMEPAGE', 'http://localhost/chatapp/');
+define('HOMEPAGE', 'http://playmate.kodedjackson.com');
 
 
 $conn=mysqli_connect(dbHost, dbUsername, dbPassword, dbDatabase);
+
+
 
 /* if(!isset($_SESSION['id'])){
     header('signin.php/');
